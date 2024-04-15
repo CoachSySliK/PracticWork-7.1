dd408f4dc8d46c4876cbfdcc0d906b7def431bb7
Юрий Бояджиев <boyadziev@mail.ru>
2024-04-15 14:25:09 +0300

    Добавьте файлы проекта.

---
+++ PracticWork-7.1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int buckwheat = 100; //гречка
+    int i;
+  
+    cout << "Вы нашли 100 кг гречки!\n";
+    
+    for (i = 1; buckwheat > 0; i++) {
+        buckwheat -= 4;
+        cout << "После " << i << " месяца у Вас останется " << buckwheat << " кг гречки\n";
+    }
+
+    cout << "На " << i << " месяц Вы останетесь без гречки\n";
+
+    return 0;
+}
